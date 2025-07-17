92a6120968ef0a2d8e5fd5013fd22260b2a6d515
Ilya Pridanov <ik.front2558988@gmail.com>
2025-07-17 17:32:54 +0300

    delete:layer

--- source/js/modules/card-text.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  const captionBtn = $('.card-text__caption-row.toggle');
-
-  captionBtn.on('click',function(){
-    $(this).toggleClass('active').next().slideToggle();
-  })
-});
--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,10 @@
 // Подключение js-файлов с помощью rigger-а
 
 // Vendor
-//= vendor/jquery-3.4.1.min.js
-//= vendor/perfect-scrollbar.js
 //= vendor/swiper.js
-//= vendor/jquery.fancybox.js
 
 // Modules
-//= modules/scroll.js
 //= modules/getHeaderMobile.js
 //= modules/getSlider.js
-//= modules/filter-toggle.js
-//= modules/range-filter.js
-//= modules/card.js
-//= modules/fancy-card.js
-//= modules/card-text.js
 
 
